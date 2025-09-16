d9fa5ed9a70d94ec7718b0f8fdd55b1a18941a71
Ikaro Henrique Martins <ikaroo.hm@gmail.com>
2025-09-16 20:50:19 -0300

    a

--- codigos-go/poo/fracao.go
+++ codigos-go/poo/fracao.go
@@ -17,7 +17,7 @@ func Somafracao(a, b, c, d int){
 		Numr = a + c
 		Denr = b
 	}
-	fmt.Printf("%d/%d somado por %d/%d é igual a %.d/%d", a, b, c, d, Numr, Denr)
+	fmt.Printf("%d/%d somado por %d/%d é igual a %.d/%d\n", a, b, c, d, Numr, Denr)
 }
 
 func Subtraifracao(a, b, c, d int){
@@ -28,21 +28,22 @@ func Subtraifracao(a, b, c, d int){
 		Numr = a - c
 		Denr = b
 	}
-	fmt.Printf("%d/%d subtraido por %d/%d é igual a %.d/%d", a, b, c, d, Numr, Denr)
+	fmt.Printf("%d/%d subtraido por %d/%d é igual a %.d/%d\n", a, b, c, d, Numr, Denr)
 }
 
 func Multiplicafracao(a, b, c, d int){
 	Numr = a * c
 	Denr = b * d
-	fmt.Printf("%d/%d multiplicado por %d/%d é igual a %.d/%d", a, b, c, d, Numr, Denr)
+	fmt.Printf("%d/%d multiplicado por %d/%d é igual a %.d/%d\n", a, b, c, d, Numr, Denr)
 }
 
 func dividirfracao(a, b, c, d int){
 	Numr = a * d
 	Denr = b * c
-	fmt.Printf("%d/%d dividido por %d/%d é igual a %.d/%d", a, b, c, d, Numr, Denr)
+	fmt.Printf("%d/%d dividido por %d/%d é igual a %.d/%d\n", a, b, c, d, Numr, Denr)
 }
 
 func main() {
 	Multiplicafracao(2, 3, 2, 7)
+	dividirfracao(3, 4, 5, 2)
 }
